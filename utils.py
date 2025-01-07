51930a9981f0f807691de7047dd308f90650e896
kuladi03 <kuladi03@gmail.com>
2025-01-07 23:51:21 +0530

    Updated the Utils

--- utils.py
+++ utils.py
@@ -2,4 +2,11 @@ opening_text = [
     "Cool, I'm on it sir.",
     "Okay sir, I'm working on it.",
     "Just a second sir.",
+    "Right away sir.",
+    "Consider it done.",
+    "I'm on it.",
+    "Sure thing, sir.",
+    "I'll take care of that.",
+    "Absolutely, sir.",
+    "Give me a moment."
 ]
